44fd46e21f51d11c7d3a0e4ad382696de1646fe2
Jay Sm <jaysmailg14@gmail.com>
2023-08-01 18:52:45 -0400

    Fixed profile router link unable to access UID

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,22 @@
       <div class="nav-link-container">
         <router-link :to="{ name: 'feed' }" class="py-1 h-full w-full flex items-center justify-center">HOME</router-link>
       </div>
-      <div class="nav-link-container">
+
+      <div v-if="FbcurrentUser" class="nav-link-container">
         <router-link :to="{ name: 'profile', params: { uid: auth.currentUser.uid } }"
           class="py-1 h-full w-full flex items-center justify-center">PROFILE</router-link>
       </div>
+      <div v-else class="nav-link-container">
+        <div class="py-1 h-full w-full flex items-center justify-center opacity-60 cursor-not-allowed select-none">PROFILE
+        </div>
+      </div>
+
       <div class="nav-link-container no-hover">
         <router-link :to="{ name: 'sign-up' }"
           class="bg-blue-400 px-2 py-1 rounded-sm text-white h-1/2 w-10/12 flex items-center justify-center">SIGN
           UP</router-link>
       </div>
+
       <div class="nav-link-container">
         <router-link :to="{ name: 'sign-in' }"
           class="px-2 py-1 rounded-sm h-full w-full flex items-center justify-center">SIGN IN</router-link>
@@ -25,7 +32,15 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { auth } from './firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+
+const FbcurrentUser = ref(null)
+
+onAuthStateChanged(auth, user => {
+  FbcurrentUser.value = user
+})
 
 </script>
 
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,6 +3,8 @@
     <h1 class="font-semibold text-4xl">Sign In</h1>
     <div class="w-5/12 min-w-[30rem]">
       <form action="" class="flex flex-col items-center mt-10 w-full">
+        <!-- <label v-if="FbError" class="text-red-400 select-none">AN ERROR HAS OCCURED</label> -->
+
         <input type="text" placeholder="Email" class="input">
         <input type="text" placeholder="Password*" class="input">
         <button class="bg-blue-400 w-[5rem] px-2 py-1 rounded-sm text-white flex items-center justify-center">Sign
@@ -13,6 +15,20 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { auth } from '../firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+
+const username = ref('')
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+const FbError = ref(true)
+
+function FbSignIn() {
+
+}
 
 </script>
 
